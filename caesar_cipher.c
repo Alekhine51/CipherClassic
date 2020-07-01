6495719968f3818511ac51efc9b1adf61af1a954
Daniel <dangilleran7@protonmail.com>
2020-07-01 01:04:05 -0400

    First ever commit to Github!

--- caesar_cipher.c
+++ caesar_cipher.c
@@ -3,5 +3,15 @@ include <stdio.h>
 // Monoalphabetic substitution cipher
 
 int main (void) {
+
+	int mod; //shift
+	char alphabet[25];
 	
+	char str[length] = text //store plaintext
+	
+	printf("Enter modulus:");
+	scanf("%d", mod);
+
+	printf("Enter plaintext:")
+	scanf("%c", text)
 }
