90b4f304f51cdf41126ba1a63843a97b9c972349
Daniel Gilleran <dangilleran7@protonmail.com>
2020-12-12 23:53:48 -0500

    Added first python script. Nonfunctional.

---
+++ caesar_cipher.py
@@ -0,0 +1,15 @@
+# Simple Caesar Cipher
+
+from string import ascii.lowercase
+
+plaintext = input("Enter plaintext or path to plaintext file:")
+
+mod = input("Enter modulus(shift number):")
+
+LETTERS = enumerate(ascii_lowercase)
+
+print(LETTERS)
+
+#for x in len(plaintext):
+    
+    
