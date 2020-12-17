f100d713825a2f1124964b2ecbef60fc42bc84a8
Daniel Gilleran <dangilleran7@protonmail.com>
2020-12-17 04:59:07 -0500

    Finished Caesar Cipher!

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -7,14 +7,27 @@ import string
 plaintext = input("Enter plaintext or path to plaintext file:")
 #add file access later
 
-mod = input("Enter shift number):")
+mod = input("Enter shift number:")
 
-numbers = list(plaintext)
+mod = int(mod)
 
-for number in range(len(numbers)):
-    numbers[number] = string.ascii_lowercase.index(numbers[number])
+cipher = list(plaintext)
 
-for number in range(len(numbers)):
-        numbers[number] = (numbers[number] + mod) % 26
+for number in range(len(cipher)):
+    cipher[number] = string.ascii_lowercase.index(cipher[number])
 
-print(numbers)
+for number in range(len(cipher)):
+        cipher[number] = (cipher[number] + mod) % 26
+
+for number in range(len(cipher)):
+    cipher[number] += 97
+    cipher[number] = chr(cipher[number])
+
+ciphertext = ''
+
+for letter in range(len(cipher)):
+    ciphertext += cipher[letter]
+
+print(ciphertext)
+
+
